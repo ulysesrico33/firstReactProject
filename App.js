5f22a170fca5ac1ade77bb31acd255a90b2fc9c3
Ulyses <ulyses.rico.2012@gmail.com>
2019-09-13 14:31:51 -0500

    A little change just to test

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ class App extends Component{
    render(){
       return(
          <div>
-            <h1>Hello World</h1>
+            <h1>Hello World, a litte change</h1>
          </div>
       );
    }
