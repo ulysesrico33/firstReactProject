125fdc83f023e6f432830a15369f5b770f5d21cb
Ulyses Rico Rea <ulyses.rico.2012@gmail.com>
2019-09-16 10:04:32 -0500

    Adding styles...

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 class App extends Component{
    render(){
+
+      var myStyle = {
+         fontSize: 100,
+         color: '#FF0000'
+      }
+
       return(
          <div>
-            <h1>Hello World, a litte change</h1>
+            <h1 style={myStyle}>Hello World, a litte change</h1>
             <p data-myattribute = "somevalue">This is the content!!!</p>
-             <p>{2+5}</p>
+             <p>{2==1 ? 'YES':'NO'}</p>
          </div>
 
       );
